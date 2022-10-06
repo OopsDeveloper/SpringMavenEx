702603e905b1030126a764fa475f35ea85f33fa9
oOpsDev <ajemxkem@gmail.com>
2022-10-06 23:11:55 +0900

    paging

--- src/test/java/com/oopsdev/article/ArticleDAOTest.java
+++ src/test/java/com/oopsdev/article/ArticleDAOTest.java
@@ -2,6 +2,7 @@ package com.oopsdev.article;
 
 import com.oopsdev.article.domain.ArticleVO;
 import com.oopsdev.article.persistence.ArticleDAO;
+import com.oopsdev.commons.paging.Criteria;
 import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -11,6 +12,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring-config/applicationContext.xml"})
@@ -46,5 +49,30 @@ public class ArticleDAOTest {
     @Test
     public void testDelete() throws Exception {
         articleDAO.delete(1001);
+    }
+
+    @Test
+    public void testListPaging() throws Exception {
+        int page = 3;
+
+        List<ArticleVO> articles = articleDAO.listPaging(page);
+
+        for (ArticleVO article : articles) {
+            logger.info(article.getArticleNo() + ":" + article.getTitle());
+        }
+    }
+
+    @Test
+    public void testListCriteria() throws Exception {
+
+        Criteria criteria = new Criteria();
+        criteria.setPage(3);
+        criteria.setPerPageNum(20);
+
+        List<ArticleVO> articles = articleDAO.listCriteria(criteria);
+
+        for (ArticleVO article : articles) {
+            logger.info(article.getArticleNo() + ":" + article.getTitle());
+        }
     }
 }
